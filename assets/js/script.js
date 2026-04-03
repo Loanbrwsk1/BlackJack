ef0f8e199423368ba737a39ce58e438a4da335b0
Loanbrwsk <Loan.Borowski@protonmail.com>
2026-04-03 15:51:04 +0200

    add split mechanic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ const settings_page = document.getElementById("settings-page");
 const nb_decks = document.getElementById("nb-decks");
 const nb_decks_slider = document.getElementById("nb-decks-selector");
 const start_button = document.getElementById("start-button");
-const dealer_hand_displayed = document.getElementById("dealer-deck");
+const dealer_hand_displayed = document.getElementById("dealer-hand");
 const dealer_score_displayed = document.getElementById("dealer-score");
-const player_hand_displayed = document.getElementById("player-deck");
+const player_hand_displayed = document.getElementById("player-hand");
 const player_score_displayed = document.getElementById("player-score");
+const split_player_hand_container = document.getElementById("split-player-hand-container");
+const split_player_hand_displayed = document.getElementById("split-player-hand");
+const split_player_score_displayed = document.getElementById("split-player-score");
+const player_hand_bet_displayed = document.getElementById("player-hand-bet");
+const split_player_hand_bet_displayed = document.getElementById("split-player-hand-bet");
 const bet_buttons = document.querySelectorAll(".bet-button");
 const play_buttons = document.querySelectorAll(".play-button");
 const play_button = document.getElementById("play");
@@ -16,7 +21,6 @@ const result_displayed = document.getElementById("result");
 const result_displayed_wrapper = document.getElementById("result-wrapper");
 const bankroll_displayed = document.getElementById("bankroll");
 const insurance_wrapper = document.getElementById("insurance-wrapper");
-const score_displayed = document.querySelectorAll(".score"); //? Juste pour cacher le background tant qu'il n'y a pas de contenu
 
 const signs = ["S", "H", "D", "C"];
 const default_deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -26,8 +30,13 @@ let dealer_hand = [];
 let dealer_score = 0;
 let player_hand = [];
 let split_player_hand = [];
+let is_spliting = false;
+let has_splited = false;
 let player_score = 0;
+let split_player_score = 0;
 let player_bet = 0;
+let total_bet = 0;
+let split_player_bet = 0;
 let insurance_bet = 0;
 let win_insurance = false;
 let bankroll = 2000;
@@ -37,6 +46,7 @@ let win_state = -1;
 let game_time = {"h" : 0, "m" : 0, "s" : 0};
 
 settings_page.style.display = "flex";
+split_player_hand_container.style.display = "none";
 
 nb_decks_slider.addEventListener('input', function() {
     nb_decks.innerText = this.value;
@@ -98,23 +108,39 @@ function scoreCalculation(deck_to_calculate)
     return score;
 }
 
-function dealCardTo(who)
+function dealCardTo(hand)
 {
-    who == "player" ? player_hand.push(deck[0]) : dealer_hand.push(deck[0]);
+    hand.push(deck[0]);
     discard.push(deck[0]);
     deck = deck.slice(1);
 }
 
-async function displayPlayerCards(nb = 0)
+function displayPlayerCards()
 {
     player_hand_displayed.innerHTML += "<img src='assets/img/cards/" + player_hand[player_hand.length - 1][0] +  player_hand[player_hand.length - 1][1]  + ".png' class='cards'>";
     player_score_displayed.innerHTML = scoreCalculation(player_hand);
 }
 
-async function displayDealerCards(nb = 0)
+function displayPlayerBet()
+{
+    player_hand_bet_displayed.innerText = player_bet;
+}
+
+function displaySplitPlayerBet()
+{
+    split_player_hand_bet_displayed.innerText = split_player_bet;
+}
+
+function displaySplitPlayerCards()
+{
+    split_player_hand_displayed.innerHTML += "<img src='assets/img/cards/" + player_hand[player_hand.length - 1][0] +  player_hand[player_hand.length - 1][1]  + ".png' class='cards'>";
+    split_player_score_displayed.innerHTML = scoreCalculation(split_player_hand);
+}
+
+function displayDealerCards(nb = 0)
 {
     if(nb != 1){
-        dealer_hand_displayed.innerHTML += "<img src='assets/img/cards/" + dealer_hand[dealer_hand.length - 1][0] + dealer_hand[dealer_hand.length - 1][1] + ".png' class='cards'>"; //? On laisse la 1ere à l'exterieur de la boucle pour que ce soit plus joli quand on retourne la 2eme carte
+        dealer_hand_displayed.innerHTML += "<img src='assets/img/cards/" + dealer_hand[dealer_hand.length - 1][0] + dealer_hand[dealer_hand.length - 1][1] + ".png' class='cards'>";
         dealer_score_displayed.innerHTML = scoreCalculation(dealer_hand);
     }
     else{
@@ -122,13 +148,19 @@ async function displayDealerCards(nb = 0)
     }
 }
 
+function displayBankroll()
+{
+    bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + total_bet + " €";
+}
+
 function bet(val)
 {
     if(val == -1){
-        bankroll += player_bet;
+        bankroll += total_bet;
+        total_bet = 0;
         player_bet = 0;
         play_button.disabled = true;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+        displayBankroll();
         bet_buttons.forEach(element => {
         if(bankroll - parseInt(element.value, 10) < 0){
             element.style.display = "flex";
@@ -136,44 +168,17 @@ function bet(val)
         });
         return;
     }
+    total_bet += val;
     player_bet += val;
     bankroll -= val;
-    bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+    displayBankroll();
+    displayPlayerBet();
     play_button.disabled = false;
     bet_buttons.forEach(element => {
         if(bankroll - parseInt(element.value, 10) < 0){
             element.style.display = "none";
         }
     });
-}
-
-function checkWin()
-{
-    if(player_hand.length == 2 && player_score == 21){
-        if(dealer_score == 21){
-            return "push";
-        }
-        return "blackjack";
-    }
-    else if(game_state == "first_round" && dealer_score == 21 && player_score < 21){
-        return "lose";
-    }
-    else if(dealer_score == player_score && player_score <= 21 && dealer_score <= 21){
-        return "push";
-    }
-    else if(player_score > 21){
-        return "bust";
-    }
-    else if(dealer_score > player_score && dealer_score <= 21){
-        return "lose";
-    }
-    else if(dealer_score < player_score){
-        return "win";
-    }
-    else if(dealer_score > 21){
-        return "win";
-    }
-    return -1;
 }
 
 async function dealerRound()
@@ -186,20 +191,30 @@ async function dealerRound()
     if(player_score < 21){
         while(dealer_score < 17){
             await sleep(500);
-            dealCardTo("dealer");
+            dealCardTo(dealer_hand);
             dealer_score = scoreCalculation(dealer_hand);
             displayDealerCards();
         }
     }
-    result();
+    if(has_splited){
+        splitResult();
+    }
+    else{
+        result();
+    }
 }
 
 function newRound()
 {
     player_hand_displayed.innerHTML = "";
     player_score_displayed.innerHTML = "";
+    player_score_displayed.style.display = "none";
+    split_player_hand_displayed.innerHTML = "";
+    split_player_score_displayed.innerHTML = "";
+    split_player_hand_container.style.display = "none";
     dealer_hand_displayed.innerHTML = "";
     dealer_score_displayed.innerHTML = "";
+    dealer_score_displayed.style.display = "none";
     bet_buttons.forEach(element => {
         element.style.display = "block";
         if(bankroll - parseInt(element.value, 10) < 0){
@@ -210,21 +225,22 @@ function newRound()
         element.style.display = "none";
         element.disabled = false;
     });
-    score_displayed.forEach(element => {
-        element.style.display = "none";
-    });
     player_bet = 0;
     player_score = 0;
     dealer_score = 0;
+    total_bet = 0;
     double_button.disabled = false;
     split_button.disabled = false;
     play_button.disabled = true;
     win_insurance = false;
+    has_splited = false;
     player_hand = [];
     dealer_hand = [];
     win_state = -1;
     game_state = "first_round";
-    bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+    displayPlayerBet();
+    displaySplitPlayerBet();
+    displayBankroll();
     if(deck.length <= 20){ //? Si il reste peu de cartes pour un tour, on rattache la défausse à la fin de deck et on mélange le tout
         deck.push(discard);
         discard = [];
@@ -241,19 +257,20 @@ async function start()
         element.style.display = "block";
         element.disabled = true;
     });
-    score_displayed.forEach(element => {
-        element.style.display = "block";
-    });
-    dealCardTo("player");
+    // dealCardTo(player_hand);
+    player_hand[0] = ["4", "H"];
+    player_score_displayed.style.display = "block";
     displayPlayerCards();
     await sleep(500);
-    dealCardTo("dealer");
+    dealCardTo(dealer_hand);
+    dealer_score_displayed.style.display = "block";
     displayDealerCards();
     await sleep(500);
-    dealCardTo("player");
+    // dealCardTo(player_hand);
+    player_hand[1] = ["4", "S"];
     displayPlayerCards();
     await sleep(500);
-    dealCardTo("dealer");
+    dealCardTo(dealer_hand);
     displayDealerCards(1);
     play_buttons.forEach(element => {
         element.disabled = false;
@@ -278,7 +295,7 @@ async function checkInsurance()
     result_displayed_wrapper.style.display = "flex";
     if(win_insurance){
         bankroll += insurance_bet + insurance_bet * 2;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+        displayBankroll();
         result_displayed.innerHTML = "Insurance :<br>+" + insurance_bet * 2 + " €";
     }
     else{
@@ -289,39 +306,99 @@ async function checkInsurance()
     win_insurance = false;
 }
 
+async function splitAction(act)
+{
+    if(act == "stand"){
+        is_spliting = false;
+        double_button.disabled = false;
+        return;
+    }
+    else if(act == "hit" && split_player_score <= 20){
+        game_state = "";
+        dealCardTo(split_player_hand);
+        split_player_score = scoreCalculation(split_player_hand);
+        displaySplitPlayerCards();
+        if(split_player_score >= 21){
+            is_spliting = false;
+            game_state = "first_round";
+            double_button.disabled = false;
+            return;
+        }
+    }
+    else if(act == "double"){
+        bankroll -= split_player_bet;
+        total_bet += split_player_bet;
+        split_player_bet *= 2;
+        displayBankroll();
+        displaySplitPlayerBet();
+        dealCardTo(split_player_hand);
+        split_player_score = scoreCalculation(split_player_hand);
+        displaySplitPlayerCards();
+        await sleep(500);
+        is_spliting = false;
+        double_button.disabled = false;
+        return;
+    }
+    if(game_state != "first_round"){
+        double_button.disabled = true;
+    }
+}
+
 async function action(act)
 {
+    if(is_spliting){
+        splitAction(act);
+        return;
+    }
     if(act == "stand"){
         dealerRound();
     }
     else if(act == "hit" && player_score <= 20){
         game_state = "";
-        dealCardTo("player");
+        dealCardTo(player_hand);
         player_score = scoreCalculation(player_hand);
         displayPlayerCards();
         if(player_score >= 21){
             dealerRound();
         }
     }
+    else if(act == "split"){
+        is_spliting = true;
+        has_splited = true;
+        total_bet += player_bet;
+        split_player_bet = player_bet;
+        bankroll -= split_player_bet;
+        displayBankroll();
+        displaySplitPlayerBet();
+        split_button.disabled = true;
+        split_player_hand.push(player_hand[1]);
+        player_hand = player_hand.splice(0, 1);
+        player_score = scoreCalculation(player_hand);
+        split_player_score = scoreCalculation(split_player_hand);
+        split_player_hand_container.style.display = "flex";
+        player_hand_displayed.innerHTML = "<img src='assets/img/cards/" + player_hand[0][0] +  player_hand[0][1]  + ".png' class='cards'>";
+        player_score_displayed.innerText = player_score;
+        split_player_hand_displayed.innerHTML = "<img src='assets/img/cards/" + split_player_hand[0][0] + split_player_hand[0][1] + ".png' class='cards'>";
+        split_player_score_displayed.innerText = split_player_score;
+    }
     else if(act == "double"){
         game_state = "";
+        total_bet += player_bet;
         bankroll -= player_bet;
         player_bet *= 2;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
-        dealCardTo("player");
+        displayBankroll();
+        displayPlayerBet();
+        dealCardTo(player_hand);
         player_score = scoreCalculation(player_hand);
         displayPlayerCards();
         await sleep(500);
         dealerRound();
     }
-    else if(act == "split"){
-        pass;
-    }
     else if(act == "insure"){
         displayInsurance();
         insurance_bet = player_bet / 2;
         bankroll -= insurance_bet;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+        displayBankroll();
         if(dealer_score == 21){
             win_insurance = true;
         }
@@ -339,6 +416,115 @@ async function action(act)
     }
 }
 
+function splitCheckWin()
+{
+    if(split_player_hand.length == 2 && split_player_score == 21){
+        if(dealer_score == 21){
+            return "push";
+        }
+    }
+    else if(game_state == "first_round" && dealer_score == 21 && split_player_score < 21){
+        return "lose";
+    }
+    else if(dealer_score == split_player_score && split_player_score <= 21 && dealer_score <= 21){
+        return "push";
+    }
+    else if(split_player_score > 21){
+        return "bust";
+    }
+    else if(dealer_score > split_player_score && dealer_score <= 21){
+        return "lose";
+    }
+    else if(dealer_score < split_player_score){
+        return "win";
+    }
+    else if(dealer_score > 21){
+        return "win";
+    }
+    return -1;
+}
+
+function checkWin()
+{
+    if(player_hand.length == 2 && player_score == 21){
+        if(dealer_score == 21){
+            return "push";
+        }
+        if(!has_splited){
+            return "blackjack";
+        }
+    }
+    else if(game_state == "first_round" && dealer_score == 21 && player_score < 21){
+        return "lose";
+    }
+    else if(dealer_score == player_score && player_score <= 21 && dealer_score <= 21){
+        return "push";
+    }
+    else if(player_score > 21){
+        return "bust";
+    }
+    else if(dealer_score > player_score && dealer_score <= 21){
+        return "lose";
+    }
+    else if(dealer_score < player_score){
+        return "win";
+    }
+    else if(dealer_score > 21){
+        return "win";
+    }
+    return -1;
+}
+
+async function splitResult(){
+    await sleep(1000);
+    win_state = splitCheckWin();
+    if(win_state == "lose"){
+        result_displayed_wrapper.style.display = "flex";
+        result_displayed.innerHTML = "<u>Split</u><br>Lose !<br>-" + split_player_bet + " €";
+        await sleep(2000);
+        result_displayed_wrapper.style.display = "none";
+        await sleep(500);
+        displayBankroll();
+    }
+    else if(win_state == "bust"){
+        result_displayed_wrapper.style.display = "flex";
+        result_displayed.innerHTML = "<u>Split</u><br>Bust !<br>-" + split_player_bet + " €";
+        await sleep(2000);
+        result_displayed_wrapper.style.display = "none";
+        await sleep(500);
+        displayBankroll();
+    }
+    else if(win_state == "win"){
+        result_displayed_wrapper.style.display = "flex";
+        result_displayed.innerHTML = "<u>Split</u><br>Win !<br>+" + split_player_bet + " €";
+        await sleep(2000);
+        result_displayed_wrapper.style.display = "none";
+        bankroll += split_player_bet + split_player_bet;
+        await sleep(500);
+        displayBankroll();
+    }
+    else if(win_state == "push"){
+        result_displayed_wrapper.style.display = "flex";
+        result_displayed.innerText = "<u>Split</u><br>Push !";
+        await sleep(2000);
+        result_displayed_wrapper.style.display = "none";
+        bankroll += split_player_bet;
+        await sleep(500);
+        displayBankroll();
+    }
+    if(bankroll <= 0){
+        result_displayed_wrapper.style.display = "flex";
+        result_displayed.innerText = "No more money !";
+        await sleep(2000);
+        result_displayed_wrapper.style.display = "none";
+        displaySettings();
+        return;
+    }
+    if(win_state != -1){
+        result();
+    }
+}
+
 async function result(){
     await sleep(1000);
     win_state = checkWin();
@@ -347,18 +533,16 @@ async function result(){
         result_displayed.innerHTML = "Lose !<br>-" + player_bet + " €";
         await sleep(2000);
         result_displayed_wrapper.style.display = "none";
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
         await sleep(500);
-        newRound();
+        displayBankroll();
     }
     else if(win_state == "bust"){
         result_displayed_wrapper.style.display = "flex";
         result_displayed.innerHTML = "Bust !<br>-" + player_bet + " €";
         await sleep(2000);
         result_displayed_wrapper.style.display = "none";
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
         await sleep(500);
-        newRound();
+        displayBankroll();
     }
     else if(win_state == "blackjack"){
         result_displayed_wrapper.style.display = "flex";
@@ -366,9 +550,8 @@ async function result(){
         await sleep(2000);
         result_displayed_wrapper.style.display = "none";
         bankroll += player_bet + player_bet * 1.5;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
         await sleep(500);
-        newRound();
+        displayBankroll();
     }
     else if(win_state == "win"){
         result_displayed_wrapper.style.display = "flex";
@@ -376,19 +559,17 @@ async function result(){
         await sleep(2000);
         result_displayed_wrapper.style.display = "none";
         bankroll += player_bet + player_bet;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
         await sleep(500);
-        newRound();
+        displayBankroll();
     }
     else if(win_state == "push"){
         result_displayed_wrapper.style.display = "flex";
         result_displayed.innerText = "Push !";
         await sleep(2000);
-        bankroll += player_bet;
-        bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
-        await sleep(1000);
         result_displayed_wrapper.style.display = "none";
-        newRound();
+        bankroll += player_bet;
+        await sleep(500);
+        displayBankroll();
     }
     if(bankroll <= 0){
         result_displayed_wrapper.style.display = "flex";
@@ -396,6 +577,10 @@ async function result(){
         await sleep(2000);
         result_displayed_wrapper.style.display = "none";
         displaySettings();
+        return;
+    }
+    if(win_state != -1){
+        newRound();
     }
 }
 
@@ -422,9 +607,11 @@ function startGame()
     dealer_hand = [];
     dealer_score = 0;
     player_hand = [];
-    split_split_deck = [];
+    split_player_hand = [];
     player_score = 0;
     player_bet = 0;
+    total_bet = 0;
+    split_player_bet = 0;
     insurance_bet = 0;
     win_insurance = false;
     bankroll = 2000;
@@ -432,7 +619,7 @@ function startGame()
     game_state = "";
     win_state = -1;
     game_time = {"h" : 0, "m" : 0, "s" : 0};
-    bankroll_displayed.innerHTML = "Bankroll : " + bankroll + " €<br>Bet : " + player_bet + " €";
+    displayBankroll();
     bet_buttons.forEach(element => {
         element.disabled = false;
     });
